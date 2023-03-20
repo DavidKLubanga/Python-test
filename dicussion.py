87cdace78c81d5773da755b134be173ddd5c6e5b
DL3 <80074622+DavidKLubanga@users.noreply.github.com>
2023-03-19 21:46:57 -0600

    Add files via upload

---
+++ dicussion.py
@@ -0,0 +1,21 @@
+#1
+
+number = int(input("Please enter a number: "))
+if number % 2 == 0:
+   print(number, "is Even")
+else:
+   print(number, "is Odd")
+   
+   
+#2
+
+x = 1
+total = 0
+
+for x in range (5):
+	number = int(input("Please enter a number: "))
+	#print(number)
+	Sum = Sum + number
+	#print(Sum)
+print ("The total is ", Sum)
+print ("The average is ", average / 5)
